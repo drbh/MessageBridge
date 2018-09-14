7a3dc57a93f7ce5ccb6a01f4bbf9de8f3b0f605c
Lucas Haber <lhaber9@gmail.com>
2018-09-13 23:57:24 -0400

    Ui changes

--- src/client/services/ColorService.js
+++ src/client/services/ColorService.js
@@ -3,12 +3,14 @@ class ColorService {
 	constructor() {
 
 		this.grey = '#898989';
-		this.extraLightGrey = '#898989BA';
 		this.lightGrey = '#949494';
+		this.extraLightGrey = '#eeeeee';
 		this.middleGrey = '#4A4A4ACC';
 		this.darkGrey = '#4A4A4A';
 		this.blue = '#4F64FF';
-		this.red = '#FF4141';
+
+		this.disabledRed = '#FF4141';
+		this.enabledGreen = '#15FF37';
 
 	}
 }
